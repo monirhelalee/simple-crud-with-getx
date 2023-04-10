492b7dfeecb1cd1819e8f6d23934db351e4f4f3c
monir <mdmonirhelale@gmail.com>
2023-04-11 04:18:16 +0600

    add products page, continue...

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,12 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const LOGIN = _Paths.LOGIN;
+  static const PRODUCTS = _Paths.PRODUCTS;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
   static const LOGIN = '/login';
+  static const PRODUCTS = '/products';
 }
