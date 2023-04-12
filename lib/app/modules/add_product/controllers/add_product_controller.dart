a38ac82d115c3a5ae63b7b98dcc96a61c8c8503c
monir <mdmonirhelale@gmail.com>
2023-04-12 16:01:01 +0600

    add product api

--- lib/app/modules/add_product/controllers/add_product_controller.dart
+++ lib/app/modules/add_product/controllers/add_product_controller.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../core/service/api_service.dart';
+import '../../../routes/app_pages.dart';
 
 class AddProductController extends GetxController {
   final TextEditingController productNameController = TextEditingController();
@@ -8,8 +13,41 @@ class AddProductController extends GetxController {
   final TextEditingController unitPriceController = TextEditingController();
   final TextEditingController mrpPriceController = TextEditingController();
   final formKey = GlobalKey<FormState>();
+  final isLoading = false.obs;
 
-  final count = 0.obs;
+  onLogin() async {
+    Map<String, dynamic> body = {
+      "name": productNameController.text,
+      "barcode": "string",
+      "description": descriptionController.text,
+      "image": "string",
+      "subCategory": 1851,
+      "brand": 1901,
+      "quantity": {
+        "quantity": 0,
+        "unit": "string",
+        "unitValue": 0,
+        "pastQuantity": 0
+      },
+      "productPrice": {
+        "price": productPriceController.text,
+        "unitPrice": unitPriceController.text,
+        "mrp": mrpPriceController.text
+      }
+    };
+    isLoading.value = true;
+    await ApiServiceHandler.post(
+        "https://secure-falls-43052.herokuapp.com/api/create-products", body,
+        onSuccess: (response) async {
+      isLoading.value = false;
+      Get.offAndToNamed(Routes.PRODUCTS);
+    }, onError: (error) {
+      isLoading.value = false;
+      log("-->$error");
+      Get.snackbar("Error!", "Something wrong, please try again.");
+    });
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -24,6 +62,4 @@ class AddProductController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
